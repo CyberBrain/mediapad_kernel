70cdb3230684442ae4b3d74b2e504309a3d18f21
ubu <ubu@ubu-VirtualBox.(none)>
2013-05-03 12:41:52 +0200

    build

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 export KBUILD_BUILD_VERSION="3.0.38"
 export KBUILD_BUILD_USER="Mediapad EX"
-export CROSS_COMPILE="/home/ubu/arm-eabi-4.4.3/bin/arm-eabi-"
+export CROSS_COMPILE="/home/ubu/toolchain/arm-eabi-4.4.0/bin/arm-eabi-"
 
-ROOTFS_PATH="/home/ubu/mediapad/ramdisk"
-ramdisk_dir=/home/ubu/mediapad/ramdisk/ramdiskjb.gz
-CMDLINE="console=console=ttyHSL0,115200,n8 androidboot.hardware=hws7300u vmalloc=578M kgsl.ptcount=16"
+ROOTFS_PATH="/home/ubu/ramdisk/"
+ramdisk_dir=/home/ubu/ramdisk/ramdisk.gz
+CMDLINE="console=console=ttyHSL0,115200,n8 androidboot.hardware=hws7300u vmalloc=578M"
 BASE="0x40300000"
 
 # Do not modify below this line
